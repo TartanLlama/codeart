6be0c68e83bfdd6d20bfc8d8d17d17e312387b5b
Simon Brand <tartanllama@gmail.com>
2019-07-29 12:53:10 +0100

    Make css smaller

--- _plugins/alt-modes.rb
+++ _plugins/alt-modes.rb
@@ -8,26 +8,26 @@ module AltModes
             else
             page.data['theme'] = 'dark'
             page.data['font'] = 'mono'
-            page.data['themefolder'] = '/'
+            page.data['themefolder'] = ''
 
             light_mono_page = Jekyll::Page.new(site, site.source, page.dir, page.name)
             light_mono_page.data['theme'] = 'light'
             light_mono_page.data['font'] = 'mono'
-            light_mono_page.data['themefolder'] = '/lightmono'
+            light_mono_page.data['themefolder'] = 'lightmono/'
             light_mono_page.data['permalink'] = File.join(File.dirname(page.url), 'lightmono', File.basename(page.url))
             new_pages << light_mono_page
 
             light_prop_page = Jekyll::Page.new(site, site.source, page.dir, page.name)
             light_prop_page.data['theme'] = 'light'
             light_prop_page.data['font'] = 'prop'
-            light_mono_page.data['themefolder'] = '/lightprop'
+            light_prop_page.data['themefolder'] = 'lightprop/'
             light_prop_page.data['permalink'] = File.join(File.dirname(page.url), 'lightprop', File.basename(page.url))
             new_pages << light_prop_page
 
             dark_prop_page = Jekyll::Page.new(site, site.source, page.dir, page.name)
             dark_prop_page.data['theme'] = 'dark'
             dark_prop_page.data['font'] = 'prop'
-            light_mono_page.data['themefolder'] = '/darkprop'
+            dark_prop_page.data['themefolder'] = 'darkprop/'
             dark_prop_page.data['permalink'] = File.join(File.dirname(page.url), 'darkprop', File.basename(page.url))
             new_pages << dark_prop_page
             end
